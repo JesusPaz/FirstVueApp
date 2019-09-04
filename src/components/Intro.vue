32fd8fde2a6a2119065131ddc3a9892fe27d15d7
Jesus Paz <jeherpabo@gmail.com>
2019-09-03 20:52:47 -0500

    add work from https://vuevixens.github.io/docs/workshop/minis/mini1.html\#supplement-2-add-animations

---
+++ src/components/Intro.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['msg'],
+    data() {
+        return{
+            msg: this.msg
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
